f20e091ccca2bca0d8fb9f93dff8c8c447b64ffd
CheateRYT <cheaterytyt@gmail.com>
2024-12-04 10:10:22 +0300

    feat: change fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,44 @@
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
-import './globals.css'
-import StoreProvider from './StoreProvider'
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import StoreProvider from "./StoreProvider";
 
 const geistSans = localFont({
-	src: './fonts/Butler.woff',
-	variable: '--font-butler',
-	weight: '100 900',
-})
+  src: "./fonts/Butler.woff",
+  variable: "--font-butler",
+  weight: "100 900",
+});
 
 const geistMono = localFont({
-	src: './fonts/Butler.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-})
+  src: "./fonts/Butler.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+const monomakh = localFont({
+  src: "./fonts/Dusha V5 Regular.ttf",
+  variable: "--font-geist-monomakh",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
-	title: 'JustStory',
-	description: 'Created by CheateRYT',
-}
+  title: "JustStory",
+  description: "Created by CheateRYT",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<StoreProvider>
-				<body
-					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-				>
-					{children}
-				</body>
-			</StoreProvider>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <StoreProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} ${monomakh.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </StoreProvider>
+    </html>
+  );
 }
